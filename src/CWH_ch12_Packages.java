a290cc6778a8ff90885ad462a452ba5f7c674d13
The Rapid Coder <md786aslam2001@gmail.com>
2024-10-07 00:24:43 +0530

    git remote add origin https://github.com/Md-Wasirul7786

--- src/CWH_ch12_Packages.java
+++ src/CWH_ch12_Packages.java
@@ -1,7 +1,12 @@
+/*
+A package in Java is used to group related classes. Think of it as a folder in a file directory. We use packages to avoid name conflicts, and to write a better maintainable code. Packages are divided into two categories:
+
+Built-in Packages (packages from the Java API)
+User-defined Packages (create your own packages)
+ */
+
 public class CWH_ch12_Packages {
     public static void main(String[] args) {
 
     }
 }
-
-
