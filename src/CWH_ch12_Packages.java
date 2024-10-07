4c36d332ab1b3acf4125fc4361928498c91e10ff
The Rapid Coder <md786aslam2001@gmail.com>
2024-10-07 14:19:15 +0530

    git remote add origin https://github.com/Md-Wasirul7786

--- src/CWH_ch12_Packages.java
+++ src/CWH_ch12_Packages.java
@@ -4,9 +4,20 @@ A package in Java is used to group related classes. Think of it as a folder in a
 Built-in Packages (packages from the Java API)
 User-defined Packages (create your own packages)
  */
+//import java.util.Scanner;
+
+//import java.util.*; //Import all classes from java.util Package
 
 public class CWH_ch12_Packages {
     public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+        java.util.Scanner sc = new java.util.Scanner(System.in); //--direct import scanner class
+//        int a = sc.nextInt();
+//        System.out.println("Taking input from user :" +a);
+        System.out.println("This is my scanner");
+        int a = sc.nextInt();
+        System.out.println(a);
+        System.out.println("done....");
 
     }
 }
