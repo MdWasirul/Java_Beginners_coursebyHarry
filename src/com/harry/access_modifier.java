1f6fa5160a1934d8edd40712e326953f7ee1bd4c
The Rapid Coder <md786aslam2001@gmail.com>
2024-10-08 19:27:42 +0530

    private member cannot be accessed  within same package

--- src/com/harry/access_modifier.java
+++ src/com/harry/access_modifier.java
@@ -6,8 +6,13 @@ class C1{
  protected  int y=15;
  int z =10;
  private  int a=25;
- public void meth1(){
+
+ public void meth1(){          //method
      System.out.println(x);
+
+     System.out.println(y);
+     System.out.println(z);
+     System.out.println(a);
  }
 }
 class C2{
@@ -16,6 +21,10 @@ class C2{
 public class access_modifier {
     public static void main(String[] args) {
 C1 access=new C1();
-access.meth1();
+//access.meth1();
+        System.out.println(access.x);
+        System.out.println(access.y);
+        System.out.println(access.z);
+//        System.out.println(access.a);  //'a' has private access in 'com.harry.C1'
     }
 }
