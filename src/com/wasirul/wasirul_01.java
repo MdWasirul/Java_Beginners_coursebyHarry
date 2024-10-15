7e88d37c4ea4828ae16a367f98a6efee04c43a4e
The Rapid Coder <md786aslam2001@gmail.com>
2024-10-15 22:31:48 +0530

    packege exe

---
+++ src/com/wasirul/wasirul_01.java
@@ -0,0 +1,15 @@
+package com.wasirul;
+
+/*
+**** Write this code in Notepad ****
+You have to create a package named com.CodeWithHarry.shape
+this package have individual classes for rectangle ,square,cylinder ,circle ,sphere.
+This class should use inheritance to properly manage the code.
+ Include method like volume surface area and Getter and Setters
+
+ */
+public class wasirul_01 {
+    public static void main(String[] args) {
+
+    }
+}
