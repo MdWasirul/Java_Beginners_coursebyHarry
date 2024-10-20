db9ae9051ba8dc17609418fa9435b3b40e1fb601
The Rapid Coder <md786aslam2001@gmail.com>
2024-10-20 23:29:29 +0530

    Thread by implementing runnable interfce

---
+++ src/CWH_ch13_2.java
@@ -0,0 +1,48 @@
+
+////Create thread using implementing runnable interface.
+
+class myThreadRunnable1 implements Runnable{
+    @Override
+    public void run(){
+        int i=0;
+        while(i<100){
+            System.out.println("I am thread 1 NOt a threat 1");
+            i++;
+        }
+    }
+}
+
+class myThreadRunnable2 implements Runnable{
+    @Override
+    public void run(){
+        int i=0;
+        while(i<100){
+            System.out.println("I am thread 2 NOt a threat 2");
+            i++;
+        }
+    }
+}
+
+public class CWH_ch13_2 {
+    public static void main(String[] args) {
+
+// myThreadRunnable1 t1= new myThreadRunnable1();
+// myThreadRunnable2 t2 =new myThreadRunnable2();
+// t1.start();
+// t2.start();
+  // when we implements Runnable interface  then we cannot directly run start() method.
+
+
+        //---------------------------------------------------------
+        myThreadRunnable1 bullet1= new myThreadRunnable1();
+        Thread gun1 =new Thread(bullet1);
+
+        myThreadRunnable2 bullet2 =new myThreadRunnable2();
+        Thread gun2 =new Thread(bullet2);
+
+        gun1.start();
+        gun2.start();
+
+    }
+
+}
